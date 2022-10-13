c8a0224a129096cc716ffe93216b7524cf2ce2d5
juanferdeleon <dele17822@uvg.edu.gt>
2022-10-12 18:25:39 -0600

    UI

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import PyQt5.QtWidgets as qtw
 import PyQt5.QtGui as qtg
-from Compiler import Compiler, MyVisitor, MyListener
+from CodeEditor import QCodeEditor
+from ScrollLabel import ScrollLabel
+# from Compiler import Compiler, MyVisitor, MyListener
 
 class MainWindow(qtw.QWidget):
 
@@ -41,54 +43,102 @@ class MainWindow(qtw.QWidget):
         
         self.layout().addWidget(self.my_buttonFile)
         
-        # Spin Box
-        self.my_text =  qtw.QTextEdit(self,
-            lineWrapMode=qtw.QTextEdit.setFixedWidth,
-            lineWrapColumnOrWidth=50,
-            placeholderText="Ingresa el codigo...",
-            readOnly=False,
-        )
-
-        self.my_text.setPlainText(self.text)
-
-        self.layout().addWidget(self.my_text)
-
+        codeEditor = QCodeEditor()
+        
+        codeEditor.setPlainText(self.text)
+        
+        self.layout().addWidget(codeEditor)
+        
         # Button
         self.my_button = qtw.QPushButton("Compilar", clicked = lambda: compilar())
         
         self.layout().addWidget(self.my_button)
 
+        # Consola
+        
+        # Creating Scroll Label
+        label_2 = ScrollLabel()
+        
+        # setting text to the label
+        label_2.setText("Consola\n\n\n\n\n\n\n\n\n\n\n")
+
+        f = label_2.font()
+        label_2.setFont(f)
+
+        # Add label to layout
+        self.layout().addWidget(label_2)
+
         # Label
-        self.label_2 = qtw.QLabel("Consola\n\n\n\n\n\n\n\n\n\n\n", self)
-        f = self.label_2.font()
         f.setPointSize(13)
         self.label_1.setFont(f)
-        self.my_text.setFont(f)
         self.my_textFile.setFont(f)
         self.my_buttonFile.setFont(f)
         self.my_button.setFont(f)
-        self.label_2.setFont(f)
-
-        self.layout().addWidget(self.label_2)
 
         def compilar():
             f = open(self.currentFile, "w")
-            f.write(self.my_text.toPlainText())
+            f.write(codeEditor.toPlainText())
             f.close()
-            compiler = Compiler()
-            compiler.compile(self.currentFile, self.my_text.toPlainText())
-            errorText = 'Consola\n'
-            for i in compiler.errors:
-                errorText += i + '\n'
-            self.label_2.setText(errorText)
+            # compiler = Compiler()
+            # compiler.compile(self.currentFile, codeEditor.toPlainText())
+            errorText = '''Consola\n
+            '''
+            # for i in compiler.errors:
+            #     errorText += i + '\n'
+            label_2.setText(errorText)
 
+            # ! TODO: APPEND STRING OF INTERMEDIATE CODE IF ANY, OTHERWISE WILL BE NONE
+            # String of intermediate code
+            i_code = None
+
+            # Si hay codigo intermedio
+            if i_code:
+                # Create new window
+                self.i_code_window = SecondaryWindow(i_code)
+                
+                self.i_code_window.show()
 
         def abrirArchivo():
             self.text=open(self.my_textFile.toPlainText()).read()
-            self.my_text.setPlainText(self.text)
+            codeEditor.setPlainText(self.text)
             self.currentFile = self.my_textFile.toPlainText()
         
         self.show()
+
+class SecondaryWindow(qtw.QWidget):
+    def __init__(self, i_code):
+        super().__init__()
+
+        # Vertical Layout
+        self.setLayout(qtw.QVBoxLayout())
+
+        self.setFixedWidth(700)
+        self.setFixedHeight(450)
+
+        # Label
+        self.label_1 = qtw.QLabel("Codigo Intermedio", self)
+
+        self.layout().addWidget(self.label_1)
+
+        # Title
+        self.setWindowTitle("Codigo Intermedio")
+
+        # Title
+        print(i_code)
+
+        # i_code
+        label = ScrollLabel()
+
+        # setting text to the label
+        label.setText("Pruebaaasss\n\n\n\n\n\n\n\n\n\n\n")
+
+        f = label.font()
+        label.setFont(f)
+
+        # Add label to layout
+        self.layout().addWidget(label)
+
+        
 
 app = qtw.QApplication([])
 mw = MainWindow('test2.cl')
